6284adcbf9bc0660b36c4b048e748cd6fc4d4087
James sandford <sandford.james@gmail.com>
2019-11-12 08:46:21 +0000

    set up testing

--- test/test.js
+++ test/test.js
@@ -3,20 +3,22 @@ var should = require("should");
 
 // This agent refers to PORT where the program is running
 
-var server = supertest.agent("http://localhost:400");
+var server = supertest.agent("http://localhost:4000");
 
 //UNIT test begin
 
 describe("SAMPLE unit test",function(){
-    //#1 shoud return home page
-    it("should return home page",function(done){
-        server
-        .get("/")
-        .expect("content-type",/text/)
-        .expect(200)
-        .end(function(err,res){
-            res.status.should.equal(200);
-            done();
-        });
+  // #1 should return home page
+  it("should return home page",function(done){
+    // calling home page
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      done();
     });
+  });
 });
